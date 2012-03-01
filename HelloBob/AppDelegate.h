844a36952b3538db2a3b1dd46386acb331703b16
Iman Sheikholmolouki <imi.sheikh@me.com>
2012-03-01 16:06:39 +0100

    Initial Commit

---
+++ HelloBob/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloBob
+//
+//  Created by Iman Sheikholmolouki on 01.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloBob/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloBob
+//
+//  Created by Iman Sheikholmolouki on 01.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
